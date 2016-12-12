48abe35324fa6fafc4c5dd3a77cc6e51ed989021
Stephen Cheung <stephencheung@Stephens-Air-2.hitronhub.home>
2016-12-11 18:43:37 -0800

    Add Handlebars templating

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-import Page from './containers/Page.js';
+var bookListingTemplate = require("./templates/product-listing.handlebars");
+
+document.addEventListener("DOMContentLoaded", function() {
+    var div = document.createElement('div');
+    div.innerHTML = bookListingTemplate({
+        username: "Stephen",
+        books: [
+            { title: "A TV", description: "a 65 inch tv" },
+            { title: "A Computer", description: "does not come with monitor" },
+            { title: "General Product" }
+        ]
+    });
+    document.body.appendChild(div);
+});
